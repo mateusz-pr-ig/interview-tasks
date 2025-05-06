c64b5e4e6e9159b44c3278c93454eac4d0fc71ac
jmatt <jmatt.technology@gmail.com>
2025-05-06 23:34:12 +0200

    Task 4 and task 5

---
+++ src/main/java/com/infogain/interview/task5/User.java
@@ -0,0 +1,22 @@
+package com.infogain.interview.task5;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    private List<Role> roles = new ArrayList<>();
+}
